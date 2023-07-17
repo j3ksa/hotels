fa5ede13f49abce6dafef1065b5cabcbf25f7a73
j3ksa <lukasz.jeksa@gmail.com>
2023-07-17 11:30:37 +0200

    [#8.1] Build fix

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,19 +1,7 @@
 import Meta from './meta'
-import { Metadata } from 'next'
 
 type Props = {
   children: React.ReactNode
-}
-
-export const metadata: Metadata = {
-  themeColor: '#E22566',
-  title: 'Hotel list challenge',
-  description: 'Job task for Guestline by Łukasz Jęksa.',
-  icons: {
-    icon: '/favicon/favicon-32x32.png',
-    shortcut: '/favicon/favicon-16x16.png',
-    apple: '/favicon/apple-touch-icon.png',
-  },
 }
 
 const Layout = ({ children }: Props) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,18 @@ import { getHotels } from './api/getHotels'
 import Loading from '../components/loading'
 import HotelList from '../components/hotelList'
 import Filter from '../components/filter'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  themeColor: '#E22566',
+  title: 'Hotel list challenge',
+  description: 'Job task for Guestline by Łukasz Jęksa.',
+  icons: {
+    icon: '/favicon/favicon-32x32.png',
+    shortcut: '/favicon/favicon-16x16.png',
+    apple: '/favicon/apple-touch-icon.png',
+  },
+}
 
 export default function Index() {
   
